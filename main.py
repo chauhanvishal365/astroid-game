750c605cda93f5b77d9487d253923cb2385ecd1b
chvgit <chauhanvishal365@gmail.com>
2026-03-04 10:04:33 +0530

    #3 fix game refersh rate

--- main.py
+++ main.py
@@ -4,21 +4,30 @@ from logger import log_state
 
 
 def main():
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    # print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    # print(f"Screen width: {SCREEN_WIDTH}")
+    # print(f"Screen height: {SCREEN_HEIGHT}")
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock =pygame.time.Clock()
+    dt = 0
 
     # Main game loop
     while True:
         log_state()
+
         for event in pygame.event.get():
            if event.type == pygame.QUIT:
-                # pygame.quit()
                 return
+
         screen.fill("black")
         pygame.display.flip()
+
+        
+        dt = clock.tick(60) / 1000
+        # print(f"Delta time: {dt} seconds")
 
 
 
